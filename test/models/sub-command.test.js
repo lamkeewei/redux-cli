bfc49b4458cf211cb62a07926a9d26a2b394b5a9
Lam Kee Wei <keewei.lam@gmail.com>
2016-08-05 00:18:14 +0800

    added tests for invalid .reduxrc file

--- test/models/sub-command.test.js
+++ test/models/sub-command.test.js
@@ -1,7 +1,26 @@
+import ProjectSettings from 'models/project-settings';
 import SubCommand from 'models/sub-command';
+import MockUI from '../helpers/mock-ui';
 
 describe('(Model) SubCommand', () => {
   const command = new SubCommand();
+
+  describe('constructor', () => {
+    it('should exit process when initialization of ProjectSettings fails', () => {
+      const options = {
+        ui: new MockUI()
+      };
+      const stub1 = sinon.stub(process, 'exit');
+      const stub2 = sinon.stub(ProjectSettings.prototype, 'loadSettings').throws();
+
+      const command = new SubCommand(options);
+      expect(command.ui.errors).to.match(/invalid/);
+      expect(stub1.calledWith(1));
+
+      stub1.restore();
+      stub2.restore();
+    });
+  });
 
   describe('subclass override intereface', () => {
     it('throws if subclass doesnt have run()', () => {
